5220eba68044bd2fa14d39f36f53e7f051aa96b7
KAnanev <russrage@gmail.com>
2021-03-09 14:25:12 +0300

    task solution pagination

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -1,5 +1,11 @@
+from csv import DictReader
+
+from urllib.parse import urlencode
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.conf import settings
 
 
 def index(request):
@@ -7,13 +13,46 @@ def index(request):
 
 
 def bus_stations(request):
-    current_page = 1
-    next_page_url = 'write your url'
+    next_page_url, prev_page_url = None, None
+
+    with open(settings.BUS_STATION_CSV, 'r', encoding='cp1251') as File:
+        reader = DictReader(File)
+        data_bus_stations = [
+            [i['Name'], i['Street'], i['District']] for i in reader
+        ]
+
+    paginator = Paginator(data_bus_stations, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    current_page = page_obj.number
+
+    if page_obj.has_previous():
+        prev_page_url = '?'.join(
+            [
+                reverse(bus_stations), urlencode(
+                    {'page': page_obj.previous_page_number()}
+                )
+            ]
+        )
+
+    if page_obj.has_next():
+        next_page_url = '?'.join(
+            [
+                reverse(bus_stations), urlencode(
+                    {'page': page_obj.next_page_number()}
+                )
+            ]
+        )
+
     return render(request, 'index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'},
-                         {'Name': 'другое название', 'Street': 'другая улица', 'District': 'другой район'}],
+        'bus_stations': [
+            {
+                'Name': page_obj[i][0],
+                'Street': page_obj[i][1],
+                'District': page_obj[i][2]
+            } for i, j in enumerate(page_obj)
+        ],
         'current_page': current_page,
-        'prev_page_url': None,
+        'prev_page_url': prev_page_url,
         'next_page_url': next_page_url,
     })
-
